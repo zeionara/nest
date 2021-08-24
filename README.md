83a20f3d538f85911d7cca84ec0311e3c1c0844e
Dmitrii <zeionara@gmail.com>
2021-08-24 23:25:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# nest
+
+**N**umerical **est**imation tool - a library containing some basic functions for approximate computation of some mathematical values.
+
+## Usage
+
+
+
+
+## Building
+
+The following command must be executed for packaging the library contents:
+
+```sh
+swift build --product nest
+```
---
+++ Tests/nestTests/nestTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import nest
+
+final class nestTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(nest().text, "Hello, World!")
+    }
+}
