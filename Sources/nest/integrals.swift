8428883f2b920765eedb0dd9ed5a8bb5c5d6a81b
Dmitrii <zeionara@gmail.com>
2021-08-25 15:40:00 +0300

    Implemented an intermediate data structure which incapsulates information about the order in which integration boundaries were given

--- Sources/nest/integrals.swift
+++ Sources/nest/integrals.swift
@@ -6,13 +6,15 @@ public enum IntegralKind {
     case mean
 }
 
+public typealias IntegrationResult<ValueType> = (value: ValueType, isPositive: Bool)
+
 public func integrate<InputType: Numeric, OutputType: Numeric>(
     _ getValue: (InputType) -> OutputType,
     from firstValue: InputType, to lastValue: InputType,
     step: InputType, zero: OutputType,
     computeSquare: (OutputType, OutputType) -> OutputType,
     identity: (InputType) -> OutputType
-) -> OutputType where InputType: Comparable {
+) -> IntegrationResult<OutputType> where InputType: Comparable {
     var isInverseOrderBounds = false // TODO: is not used
     var leftBoundary = firstValue
     var rightBoundary = lastValue
@@ -40,13 +42,13 @@ public func integrate<InputType: Numeric, OutputType: Numeric>(
 
     // let step = (rightBoundary - leftBoundary) / nIntervals
     
-    return result // identity(lastValue - firstValue) * getValue(firstValue)
+    return IntegrationResult(value: result, isPositive: !isInverseOrderBounds) // identity(lastValue - firstValue) * getValue(firstValue)
 }
 
 public func integrate<Type: Numeric>(
     _ getValue: (Type) -> Type,
     from firstValue: Type, to lastValue: Type, step: Type, zero: Type, computeSquare: (Type, Type) -> Type
-) -> Type where Type: Comparable {
+) -> IntegrationResult<Type> where Type: Comparable {
     return integrate(
         getValue, from: firstValue, to: lastValue, step: step, zero: zero, computeSquare: computeSquare
     ) {
@@ -59,8 +61,8 @@ public func integrate(
     from firstValue: Double, to lastValue: Double, precision nIntervals: Int = 10, kind: IntegralKind = .right
 ) -> Double {
     let step = abs(lastValue - firstValue) / Double(nIntervals)
-    // print(step)
-    return integrate(
+    
+    let result = integrate(
         getValue, from: firstValue, to: lastValue, step: step, zero: 0.0
     ) { (leftHeight: Double, rightHeight: Double) in
         if kind == .right {
@@ -72,4 +74,6 @@ public func integrate(
             return (leftHeight + rightHeight) / 2.0 * step
         }
     }
+
+    return result.isPositive ? result.value : -result.value
 }
