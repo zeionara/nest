5b05e8199306fb440971812789d72ada0cafa882
Dmitrii <zeionara@gmail.com>
2021-09-10 02:24:20 +0300

    Updated dependencies list

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
+        .package(url: "https://github.com/paiv/swift-pcg-random.git", .upToNextMajor(from: "1.0.0"))
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
@@ -21,7 +22,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "nest",
-            dependencies: []
+            dependencies: [
+                .product(name: "PcgRandom", package: "swift-pcg-random")
+            ]
         ),
         .testTarget(
             name: "nestTests",
